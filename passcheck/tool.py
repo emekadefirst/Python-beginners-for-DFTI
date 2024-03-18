2cab8df32a60a78c64a33fdd919a4f183a90b2a8
madefromjames <madefromjames@gmail.com>
2024-03-18 16:54:26 +0100

    James While Loop

--- passcheck/tool.py
+++ passcheck/tool.py
@@ -3,12 +3,12 @@ from gen import gen_pass
 
 
 def check():
-    n = str(input("Enter password let me check\n>> "))
-    if any(char.isupper() for char in n) and any(char.isdigit() for char in n) and any(char in s.punctuation for char in n):
-        print("Password is strong")
-
-    else:
-        return print(f"Password is weak\nWe suggest {gen_pass()}")
+    while True:
+        n = str(input("Enter password let me check\n>> "))
+        if any(char.isupper() for char in n) and any(char.isdigit() for char in n) and any(char in s.punctuation for char in n):
+            print("Password is strong")
+            break
+        else:
+            print(f"Password is weak\nWe suggest {gen_pass()}")
         
-
 check()
