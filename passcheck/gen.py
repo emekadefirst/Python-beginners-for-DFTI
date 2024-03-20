a92b96c907815b1a1c11a109f5289dfca84b80b2
TopNaid <habeebjinad6@gmail.com>
2024-03-20 10:38:13 +0100

    added while loop and integer to the genpass function

--- passcheck/gen.py
+++ passcheck/gen.py
@@ -5,12 +5,15 @@ def gen_pass():
     ul = s.ascii_uppercase
     ll = s.ascii_lowercase
     punctuation = s.punctuation
+    digi = s.digits
+    
 
     first_letter = ''.join(r.sample(ul, 4))
     second_letter = ''.join(r.sample(ll, 4))
+    third_digi = ''.join(r.sample(digi,4))
 
     comma = ''.join(r.sample(punctuation, 4))
-    all_value = first_letter + second_letter + comma
+    all_value = first_letter + second_letter + comma + third_digi
     password = list(all_value)
     r.shuffle(password)
 
